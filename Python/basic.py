b288c7f9cf1fe39be90b0a950201344ca521bcb0
Samuel King <samueljamesking94@gmail.com>
2024-09-25 13:23:19 +0100

    basic python practise

---
+++ Python/basic.py
@@ -0,0 +1,31 @@
+# # str1 = "My name is {firstName}, I am {age} years old".format(firstName = 'Samuel', age = 30)
+# # str1 = "My name is {0}, I am {1} years old".format('Samuel', 30)
+# str1 = "My name is {}, I am {} years old".format('Samuel', 30)
+
+# print(str1)
+
+# # Important string functions
+
+# str2 = '5473sdfg'
+# print(str2.isdigit())
+
+# text = 'Python is annoying'
+# print(text.split())
+
+# text1 = 'Spiderman, Batman, Superman'
+# print(text1.rsplit(','))
+
+# print(text1.find('Batman'))
+
+strings = ['HELLO', 'WORLD']
+strings.reverse()
+print(" ".join(strings))
+
+
+
+
+
+
+
+
+
--- Python/test.py
+++ Python/test.py
@@ -1,79 +1,116 @@
-# value = input("enter a value")
+# # value = input("enter a value")
 
-# print(value)
+# # print(value)
 # python syntax uses whitespace and indentations to seperate parts of code. It also utilises colons rather than semicolons to separate statements
 
 
-# value = int(input("enter a value"))
-# def printIncrements():
+# # value = int(input("enter a value"))
+# # def printIncrements():
     
-#     i=0
-#     max=100
-#     while (i <= max):
-#         print(i)
-#         i=i+value
+# #     i=0
+# #     max=100
+# #     while (i <= max):
+# #         print(i)
+# #         i=i+value
 
 
-# printIncrements()
+# # printIncrements()
 
-# num1 = int(input("enter a number"))
-# num2 = int(input("enter another number"))
+# # num1 = int(input("enter a number"))
+# # num2 = int(input("enter another number"))
 
-# print(num1+num2)
+# # print(num1+num2)
 
-# a = int(input("enter a number"))
-# b = int(input("enter another number"))
-# c = int(input("enter another number"))
+# # a = int(input("enter a number"))
+# # b = int(input("enter another number"))
+# # c = int(input("enter another number"))
 
-# def printTriangleArea():
-#     s = (a + b + c) / 2
-#     area = (s * (s-a) * (s-b) * (s-c)) ** 0.5
+# # def printTriangleArea():
+# #     s = (a + b + c) / 2
+# #     area = (s * (s-a) * (s-b) * (s-c)) ** 0.5
 
-#     print(area)
+# #     print(area)
 
-# printTriangleArea()
+# # printTriangleArea()
 
-calcType = int(input("enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"))
-num1 = int(input("enter a number"))
-num2 = int(input("enter another number"))
+# # calcType = int(input("enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"))
+# # num1 = int(input("enter a number"))
+# # num2 = int(input("enter another number"))
 
 
 # below is an example of a calculator using if/else statements in python
-def calc():
-    if calcType == 1:
-        print(num1 + num2)
-    elif calcType == 2:
-        print(num1 - num2)
-    elif calcType == 3:
-        print(num1 * num2)
-    elif calcType == 4:
-        print(num1 / num2)
-    else:
-        print("invalid input")
+# # def calc():
+# #     if calcType == 1:
+# #         print(num1 + num2)
+# #     elif calcType == 2:
+# #         print(num1 - num2)
+# #     elif calcType == 3:
+# #         print(num1 * num2)
+# #     elif calcType == 4:
+# #         print(num1 / num2)
+# #     else:
+# #         print("invalid input")
 
-# calc()
+# # # calc()
 
 
 # below is an example of a calculator using a switch statement in python
-def calc2():
-    match calcType:
-        case 1:
-            print(num1 + num2)
-        case 2:
-            print(num1 - num2)
-        case 3:
-            print(num1 * num2)
-        case 4:
-            print(num1 / num2)
-        case _:
-            print("invalid input")
+# # def calc2():
 
-
-calc2()
-
-
-4
-
-
-
+# #     match calcType:
+# #         case 1:
+# #             print(num1 + num2)
+# #         case 2:
+# #             print(num1 - num2)
+# #         case 3:
+# #             print(num1 * num2)
+# #         case 4:
+# #             print(num1 / num2)
         
+# # calc2()
+
+# best practise to declare functions to use in multiple places, whileTrue statmertns used to keep the program running.
+# def add(x, y):
+#     return x + y
+
+# def subtract(x, y):
+#     return x - y
+
+# def multiply(x, y):
+#     return x * y
+
+# def divide(x, y):
+#     return x / y    
+
+# print("please select operation to execute")
+# print("1. Add")
+# print("2. Subtract")
+# print("3. Multiply")
+# print("4. Divide")
+
+# while True:
+#     choice = input("enter choice(1/2/3/4): ")
+
+#     if choice in ('1', '2', '3', '4'):
+#         try:
+#             num1 = float(input("enter first number: "))
+#             num2 = float(input("enter second number: "))
+#         except ValueError:
+#             print("Invalid input - please choose a number between 1 and 4")
+#             continue
+
+#         if choice == '1':
+#             print(num1, "+", num2, "=", add(num1, num2))
+
+#         elif choice == '2':
+#             print(num1, "-", num2, "=", subtract(num1, num2))
+
+#         elif choice == '3':
+#             print(num1, "*", num2, "=", multiply(num1, num2))
+
+#         elif choice == '4':
+#             print(num1, "/", num2, "=", divide(num1, num2))
+#         break
+#     else:
+#         print("invalid input")
+
