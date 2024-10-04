94bdff5a1e5dea14688b884860979fcd81a39438
Samuel King <samueljamesking94@gmail.com>
2024-10-04 10:11:33 +0100

    specific line selectors

--- Python/fileHandling/session3.py
+++ Python/fileHandling/session3.py
@@ -18,9 +18,22 @@
 # fp.write('Testing python file handling skills: open and insert this text in the file')
 # fp.close()
 
-fp = open('C:/Users/king_/OneDrive/Desktop/Bootcamp Assignments/Python/fileHandling/test.txt', 'a')
-fp.write('\nInsert to end of file')
-fp.close()
+# fp = open('C:/Users/king_/OneDrive/Desktop/Bootcamp Assignments/Python/fileHandling/test.txt', 'a')
+# fp.write('\nInsert to end of file')
+# fp.close()
 
+# with open(r'Python/fileHandling/test.txt', 'a') as fp:
+#     fp.write('\nInsert to end of file using with statement')
 
- 
+with open(r'Python/fileHandling/test.txt', 'r') as fp:
+    line_numbers = [0, 1]
+
+    lines = []
+
+    for i, line in enumerate(fp):
+        if i in line_numbers:
+            lines.append(line)
+        elif i > 1:
+            break
+    
+print(lines)
